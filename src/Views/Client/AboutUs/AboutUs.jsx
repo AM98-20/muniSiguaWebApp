3eae6ccba5e8b264b87cdea8cc4a711e4349b101
Leeroy Rivera <leeroya506@gmail.com>
2022-07-20 13:54:27 -0600

    hhh

--- src/Views/Client/AboutUs/AboutUs.jsx
+++ src/Views/Client/AboutUs/AboutUs.jsx
@@ -3,14 +3,16 @@ import "./AboutUs.css";
 
 const AboutUs = () => {
   return (
-    <div className="aboutMain">
+    <div className="main">
       <div class="wrapper">
-        <header class="header">Conocenos e imagen de fondo</header>
-        <aside class="aside aside-1">Mision</aside>
-        <aside class="aside aside-2">Vision</aside>
+        <div class="backgroundTitle">
+          <h1 className="title">CONOCENOS</h1>
+        </div>
+          <aside class="aside aside-1">Mision</aside>
+          <aside class="aside aside-2">Vision</aside>
         <article class="wrapperMain">
           <h1>Nuestra Historia</h1>
-          <p>
+          <p className="paragraph">
             Fue fundado en 1689; en el recuento de población de 1791 era
             cabecera de curato; el 14 de abril de 1861 se le dio categoría de
             municipio con el nombre ale San José de Siguatepeque. Se le dio el
@@ -20,7 +22,7 @@ const AboutUs = () => {
             se devuelve la autonomía municipal. El origen de su nombre
             significa: "CERRO DE LA MUJER"
           </p>
-          <p>
+          <p className="paragraph">
             El actual emplazamiento de siguatepeque era habitado por indígenas
             de la etnia lenca que vivían en casas de paja en la rivera de ríos y
             riachuelos. De este grupo humano asi como de las etnias mayas han
@@ -29,26 +31,37 @@ const AboutUs = () => {
             de diferentes formas y tamaños.
           </p>
           <div className="imageContainer">
-             <img class="image1" src="/about/sample.png" alt="1" width="200" height="200"  />
-             <img class="image1" src="/about/sample.png" alt="1" width="200" height="200"  />
+            <img
+              class="image1"
+              src="/about/sample.png"
+              alt="1"
+              width="200"
+              height="200"
+            />
+            <img
+              class="image1"
+              src="/about/sample.png"
+              alt="1"
+              width="200"
+              height="200"
+            />
           </div>
-          <p>
-            Siguatepeque se cree que fue fundada
-            inicialmente por indios de origen lenca, por el año de 1689 y ya
-            para el año de 1788 , Siguetepeque formaba parte de un cuarto de
-            seis cofradías y contaba con las siguientes aldeas: Jaitique,
-            Meambar y Taulabe el libro de registros mas antiguos de la época que
-            se ha encontrado data de los años 1850, formando este parte del
-            archivo de la municipalidad el 14 de abril de 1861 se le dio a
-            siguatepeque la categoría de municipio del departamento de
-            comayagua, inicialmente se le dio en nombre de San Jose de
-            Siguatepeque para el año de 1889 pierde como municipio la aldea de
-            Meambar la que se constituye a la vez como municipio ese mismo año
-            (1889), Siguatepeque se convierte en capital religiosa del país , al
-            instante en el lugar el obispo de Honduras Fray Manuel Francisco
-            Velez quien había adquirido el inmueble del que fue el antiguo
-            edificio del instituto Genaro Muñoz Hernández el cual era en esos
-            tiempos casa de retiros para los sacerdotes de su parroquia.
+          <p className="paragraph">
+            Siguatepeque se cree que fue fundada inicialmente por indios de
+            origen lenca, por el año de 1689 y ya para el año de 1788 ,
+            Siguetepeque formaba parte de un cuarto de seis cofradías y contaba
+            con las siguientes aldeas: Jaitique, Meambar y Taulabe el libro de
+            registros mas antiguos de la época que se ha encontrado data de los
+            años 1850, formando este parte del archivo de la municipalidad el 14
+            de abril de 1861 se le dio a siguatepeque la categoría de municipio
+            del departamento de comayagua, inicialmente se le dio en nombre de
+            San Jose de Siguatepeque para el año de 1889 pierde como municipio
+            la aldea de Meambar la que se constituye a la vez como municipio ese
+            mismo año (1889), Siguatepeque se convierte en capital religiosa del
+            país , al instante en el lugar el obispo de Honduras Fray Manuel
+            Francisco Velez quien había adquirido el inmueble del que fue el
+            antiguo edificio del instituto Genaro Muñoz Hernández el cual era en
+            esos tiempos casa de retiros para los sacerdotes de su parroquia.
           </p>
         </article>
       </div>
